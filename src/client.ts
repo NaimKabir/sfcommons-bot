a856165551ad3515ef2d0d8aa9640053e170e162
NaimKabir <kabir.naim@gmail.com>
2022-12-13 23:22:50 -0800

    fmt

--- src/client.ts
+++ src/client.ts
@@ -1,10 +1,7 @@
-import { CONFIG } from './config';
-import { WebClient } from '@slack/web-api';
-import { tenRetriesInAboutThirtyMinutes } from '@slack/web-api/dist/retry-policies';
+import { CONFIG } from "./config";
+import { WebClient } from "@slack/web-api";
+import { tenRetriesInAboutThirtyMinutes } from "@slack/web-api/dist/retry-policies";
 
-export const CLIENT = new WebClient(
-  CONFIG.botToken,
-  {
-    retryConfig: tenRetriesInAboutThirtyMinutes,
-  }
-)
+export const CLIENT = new WebClient(CONFIG.botToken, {
+  retryConfig: tenRetriesInAboutThirtyMinutes,
+});
--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export const CONFIG = {
   sparkConnectionsChannelId: "C04AMJBCU4F", // #13-spark-connections
   blackList: [
     "U042H5S0K7E", // the Admin account
-    "U04BEPGCU0H" // legacy Donut user
+    "U04BEPGCU0H", // legacy Donut user
   ],
   targetGroupSize: 4,
-}
+};

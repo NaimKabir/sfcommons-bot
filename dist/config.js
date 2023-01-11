9f74313a238670d62680d94b1ca01ee75569a470
NaimKabir <kabir.naim@gmail.com>
2023-01-10 20:13:28 -0800

    update to public qa channel

--- dist/config.js
+++ dist/config.js
@@ -4,7 +4,7 @@ exports.ENVIRONMENTS = exports.CONFIG = void 0;
 exports.CONFIG = {
     botToken: process.env.SLACK_BOT_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET,
-    errorChannelId: "C04HQ77DW1Z",
+    errorChannelId: "C04J2SP748M",
     sparkConnectionsChannelId: "C04AMJBCU4F",
     blackList: [
         "U042H5S0K7E",
--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const CONFIG = {
   botToken: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  errorChannelId: "C04HQ77DW1Z", // #qa-channel-for-kabir
+  errorChannelId: "C04J2SP748M", // #x0testing
   sparkConnectionsChannelId: "C04AMJBCU4F", // #13-spark-connections
   blackList: [
     "U042H5S0K7E", // the Admin account
